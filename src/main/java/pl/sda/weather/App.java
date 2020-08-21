ecee944edc5ecf4fcec080f3070693af2caedd3c
Bartosz <bartoszpawlikowsky@gmail.com>
2020-08-21 09:26:37 +0200

    merging upgrade

--- src/main/java/pl/sda/weather/App.java
+++ src/main/java/pl/sda/weather/App.java
@@ -1,31 +1,19 @@
 package pl.sda.weather;
 
-<<<<<<< HEAD
 import com.sun.javafx.scene.SceneHelper;
 import com.sun.javafx.scene.SceneUtils;
-=======
-
->>>>>>> 7bc9f7f37b9166336a30117b48ac23da009fb132
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7bc9f7f37b9166336a30117b48ac23da009fb132
 public class App extends Application
 {
     public static void main( String[] args )
     {
         launch(args);
-<<<<<<< HEAD
         System.out.println( "JavaFX closed" );
-=======
-        System.out.println( "Hello World!" );
->>>>>>> 7bc9f7f37b9166336a30117b48ac23da009fb132
     }
 
     @Override
